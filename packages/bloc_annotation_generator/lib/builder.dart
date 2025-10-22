ca5345f716a2794ef9af3d062bc27abbe20337ff
maranix <ramanverma4183@gmail.com>
2025-10-22 11:54:31 +0530

    feat (bloc_annotation_generator): add builder and generator placeholders for StateClass & StateEnum

--- packages/bloc_annotation_generator/lib/builder.dart
+++ packages/bloc_annotation_generator/lib/builder.dart
@@ -1,6 +1,13 @@
-import 'package:bloc_annotation_generator/src/cubit_generator.dart';
+import 'package:bloc_annotation_generator/src/generators/cubit_generator.dart';
+import 'package:bloc_annotation_generator/src/generators/state_generator.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder cubitClassBuilder(BuilderOptions options) =>
     SharedPartBuilder([CubitClassGenerator()], "cubitClass");
+
+Builder stateClassBuilder(BuilderOptions options) =>
+    SharedPartBuilder([StateClassGenerator()], "stateClass");
+
+Builder stateEnumBuilder(BuilderOptions options) =>
+    SharedPartBuilder([StateEnumGenerator()], "stateEnum");
---
+++ packages/bloc_annotation_generator/lib/src/generators/state_generator.dart
@@ -0,0 +1,29 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:bloc_annotation/bloc_annotation.dart';
+import 'package:bloc_annotation_generator/src/element_code_generator.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+
+final class StateClassGenerator extends GeneratorForAnnotation<StateClass> {
+  @override
+  String generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    final elementGenerator = ClassCodeGenerator(element);
+
+    return super.generateForAnnotatedElement(element, annotation, buildStep);
+  }
+}
+
+final class StateEnumGenerator extends GeneratorForAnnotation<StateEnum> {
+  @override
+  String generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    return super.generateForAnnotatedElement(element, annotation, buildStep);
+  }
+}
